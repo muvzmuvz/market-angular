d9ed34a8a2b0d8791770f8f50d7c6cdd67d10493
Mikhaik <moishemg@vk.com>
2025-06-18 14:24:36 +0300

    fix

--- src/app/pages/product-detail/product-detail.ts
+++ src/app/pages/product-detail/product-detail.ts
@@ -7,6 +7,7 @@ import { Navbar } from 'src/app/components/navbar/navbar';
 import { Router } from '@angular/router';
 import { TuiAvatar } from '@taiga-ui/kit';
 import { TuiBadge } from '@taiga-ui/kit';
+import { TuiButton } from '@taiga-ui/core';
 
 interface ProductImage {
   path: string;
@@ -42,7 +43,7 @@ interface Comment {
   standalone: true,
   templateUrl: './product-detail.html',
   styleUrls: ['./product-detail.less'],
-  imports: [CommonModule, FormsModule, Navbar, TuiLoader, TuiAvatar, TuiBadge]
+  imports: [CommonModule, FormsModule, Navbar, TuiLoader, TuiAvatar, TuiBadge, TuiButton]
 })
 export class ProductDetailComponent implements OnInit {
   product: Product | null = null;
@@ -91,7 +92,9 @@ export class ProductDetailComponent implements OnInit {
         images: [
           { path: 'https://ae04.alicdn.com/kf/Sf73c6b0b761343abb82901fa276c7c36x.jpg_480x480.jpg' },
           { path: 'https://c1.cprnt.com/storage/p/t1/aw/lwh/8d/bcc/c35e91145a61f1f09fe24b3b31d/bb93552490a04d75aea502d2c1ed3257.webp' },
-          { path: 'https://modano.ru/wa-data/public/shop/products/08/15/1508/images/203949/203949.635x953@2x.jpg' }
+          { path: 'https://modano.ru/wa-data/public/shop/products/08/15/1508/images/203949/203949.635x953@2x.jpg' },
+          { path: 'https://modano.ru/wa-data/public/shop/products/08/15/1508/images/203949/203949.635x953@2x.jpg' },
+          { path: 'https://modano.ru/wa-data/public/shop/products/08/15/1508/images/203949/203949.635x953@2x.jpg' },
         ],
         colors: ['Красный', 'Чёрный', 'Белый'],
         sizes: ['S', 'M', 'L', 'XL'],
@@ -200,4 +203,6 @@ export class ProductDetailComponent implements OnInit {
   viewSimilar(): void {
     alert('Переход к похожему товару...');
   }
+
+  
 }
